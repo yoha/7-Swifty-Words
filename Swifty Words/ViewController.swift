303ff64f653c921429d54b506174196242beeae4
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-08-17 16:31:19 +0700

    Added the game's logic. Configured the buttons and labels.

--- Swifty Words/ViewController.swift
+++ Swifty Words/ViewController.swift
@@ -12,7 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        for subView in self.view.subviews {
+            if subView.tag == 1001 {
+                let btn = subView as! UIButton
+                self.letterButtons.append(btn)
+                btn.addTarget(self, action: "letterTapped:", forControlEvents: UIControlEvents.TouchUpInside)
+            }
+        }
+        self.loadLevel()
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +42,60 @@ class ViewController: UIViewController {
     
     @IBAction func clearButtonTapped(sender: UIButton) {
     }
+    
+    // MARK: - Stored Properties
+    
+    var letterButtons = [UIButton]()
+    let activatedButtons = [UIButton]()
+    var solutions = Array<String>()
+    
+    var gameScore = 0
+    var gameLevel = 1
+    
+    // MARK: - Custom Methods
+    
+    func loadLevel() {
+        var clueString = String()
+        var solutionString = ""
+        var letterBits = [String]()
+        var gameLevelContents: NSString!
 
+        if let gameLevelFilePath = NSBundle.mainBundle().pathForResource("gameLevel\(self.gameLevel)", ofType: "txt") {
+            do {
+                gameLevelContents = try NSString(contentsOfFile: gameLevelFilePath, usedEncoding: nil)
+            }
+            catch {
+                print("Something awful has just happened.")
+            }
+            var gameLevelContentsSeparated: [String] = gameLevelContents.componentsSeparatedByString("\n")
+            gameLevelContentsSeparated.shuffle()
+            
+            for (index, eachGameLevelContentsSeparated) in gameLevelContentsSeparated.enumerate() {
+                let answerAndClue = eachGameLevelContentsSeparated.componentsSeparatedByString(": ")
+                let answer = answerAndClue[0]
+                let clue = answerAndClue[1]
+                clueString += "\(index + 1). \(clue)\n"
+                
+                let solutionWord = answer.stringByReplacingOccurrencesOfString("|", withString: "")
+                solutionString += "\(solutionWord.characters.count) letters\n"
+                self.solutions.append(solutionWord)
+                
+                let bits = answer.componentsSeparatedByString("|")
+                letterBits += bits
+            }
+        }
+        
+        print("clueString: \(clueString)") // <--- to be erased
+        print("solutionString: \(solutionString)") // <-- to be erased
+        self.cluesLabel.text = clueString.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        self.answersLabel.text = solutionString.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        letterBits.shuffle()
+        self.letterButtons.shuffle()
+        if letterBits.count == self.letterButtons.count {
+            for index in 0 ..< letterBits.count {
+                self.letterButtons[index].setTitle(letterBits[index], forState: UIControlState.Normal)
+            }
+        }
+    }
 }
 
